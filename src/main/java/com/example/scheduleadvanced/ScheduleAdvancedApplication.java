ff6591768641f1d6551676847f9d9177546c78ae
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-17 20:32:11 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/scheduleadvanced/ScheduleAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduleadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleAdvancedApplication.class, args);
+    }
+
+}
