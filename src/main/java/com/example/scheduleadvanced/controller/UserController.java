0737ee7e2f1d4dd64ea42848051e2f094470b252
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-20 16:18:06 +0900

    유저 생성 기능 구현

--- src/main/java/com/example/scheduleadvanced/controller/UserController.java
+++ src/main/java/com/example/scheduleadvanced/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.example.scheduleadvanced.controller;
 
+import com.example.scheduleadvanced.dto.Schedule.UpdateScheduleRequest;
+import com.example.scheduleadvanced.dto.User.CreateUserRequest;
+import com.example.scheduleadvanced.dto.User.CreateUserResponse;
+import com.example.scheduleadvanced.entity.User;
 import com.example.scheduleadvanced.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity
+    public ResponseEntity<CreateUserResponse> save(@RequestBody CreateUserRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(request));
+    }
 }
--- src/main/java/com/example/scheduleadvanced/service/UserService.java
+++ src/main/java/com/example/scheduleadvanced/service/UserService.java
@@ -1,11 +1,25 @@
 package com.example.scheduleadvanced.service;
 
+import com.example.scheduleadvanced.dto.User.CreateUserRequest;
+import com.example.scheduleadvanced.dto.User.CreateUserResponse;
+import com.example.scheduleadvanced.entity.User;
 import com.example.scheduleadvanced.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional
+    public CreateUserResponse save(CreateUserRequest request) {
+        User user = new User(request.getName(), request.getEmail());
+        return new CreateUserResponse(
+                user.getName(),
+                user.getEmail(),
+                user.getCreateAt(),
+                user.getUpdateAt());
+    }
 }
