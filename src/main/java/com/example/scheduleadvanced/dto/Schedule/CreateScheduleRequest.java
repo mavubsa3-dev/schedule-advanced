1e075d3d98a7a0a912e988059658ab32831ec0eb
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-21 17:28:00 +0900

    Schedule DTO 속성들 Validation 추가

--- src/main/java/com/example/scheduleadvanced/dto/Schedule/CreateScheduleRequest.java
+++ src/main/java/com/example/scheduleadvanced/dto/Schedule/CreateScheduleRequest.java
@@ -1,12 +1,20 @@
 package com.example.scheduleadvanced.dto.Schedule;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class CreateScheduleRequest {
+
+    @NotBlank(message = "일정 제목을 입력하세요")
+    @Size(min = 4)
     private String title;
+
+    @NotBlank(message = "내용을 입력하세요")
+    @Size(max = 1500)
     private String content;
 
 }
--- src/main/java/com/example/scheduleadvanced/dto/Schedule/UpdateScheduleRequest.java
+++ src/main/java/com/example/scheduleadvanced/dto/Schedule/UpdateScheduleRequest.java
@@ -1,10 +1,16 @@
 package com.example.scheduleadvanced.dto.Schedule;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateScheduleRequest {
-    private Long userId;
+    @NotBlank(message = "일정 제목을 입력하세요")
+    @Size(min = 4)
     private String title;
+
+    @NotBlank(message = "내용을 입력하세요")
+    @Size(max = 1500)
     private String content;
 }
